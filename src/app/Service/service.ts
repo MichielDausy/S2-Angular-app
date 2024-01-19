674dcca9b51aa1aae259c27f4ba2248c944ee56a
Thomas De Ceuster <r0882755@student.thomasmore.be>
2024-01-19 16:11:07 +0100

    Update service.ts

--- src/app/Service/service.ts
+++ src/app/Service/service.ts
@@ -12,6 +12,7 @@ export class Service {
   private _url: string = "";
 
   constructor(private http: HttpClient) { }
+  
 
   getAnomalies(): Observable<Anomaly[]> {
     return this.http.get<Anomaly[]>(this._url);
