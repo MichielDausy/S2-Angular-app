08de13e655a883870fa43eab978860759840ad63
michieldausy <michiel_dausy@hotmail.com>
2024-01-23 10:00:16 +0100

    API endpoints ready

--- src/app/Service/service.ts
+++ src/app/Service/service.ts
@@ -14,46 +14,41 @@ import { Sign } from '../Models/sign';
 
 export class Service {
 
-  private _url: string = "http://localhost:3000/";
+  private _url: string = "http://localhost:8081/api/";
 
   constructor(private http: HttpClient) { }
   
 
   getAnomalies(): Observable<Anomaly[]> {
-    return this.http.get<Anomaly[]>(this._url+"anomalies");
+    return this.http.get<Anomaly[]>(this._url+"anomalies/all");
   }
 
   getAnomalyById(id: number): Observable<Anomaly> {
-    return this.http.get<Anomaly>(this._url+"anomalies/"+id);
+    return this.http.get<Anomaly>(`${this._url}anomalies?id=${id}`);
   }
 
   getTrainTracks(): Observable<Traintrack[]> {
-    return this.http.get<Traintrack[]>(this._url+"trainTracks");
+    return this.http.get<Traintrack[]>(this._url+"trainTracks/all");
   }
 
   getTrains(): Observable<Train[]> {
-    return this.http.get<Train[]>(this._url+"trains");
+    return this.http.get<Train[]>(this._url+"trains/all");
   }
 
-  getSigns(): Observable<Sign[]> {
+  /*getSigns(): Observable<Sign[]> { won't use this anymore because we will not specify which sign it is
     return this.http.get<Sign[]>(this._url+"signs");
-  }
+  }*/
 
   getAnomalyTypes(): Observable<Anomalytype[]> {
-    return this.http.get<Anomalytype[]>(this._url+"anomalyTypes");
+    return this.http.get<Anomalytype[]>(this._url+"types/all");
   }
 
   getCountries(): Observable<Country[]> {
-    return this.http.get<Country[]>(this._url+"countries");
+    return this.http.get<Country[]>(this._url+"countries/all");
   }
 
-  markAnomalyAsFixedById(id: number): Observable<Anomaly> {
-    return this.http.patch<Anomaly>(this._url+"anomalies/"+id, {isFixed: true});
+  markAnomaly(id: number, isFixed: boolean, isFalse: boolean): Observable<Anomaly> {
+    return this.http.put<Anomaly>(`${this._url}anomalies/mark`, { id, isFixed, isFalse });
   }
-
-  markAnomalyAsFalseById(id: number): Observable<Anomaly> {
-    return this.http.patch<Anomaly>(this._url+"anomalies/"+id, {isFalse: true});
-  }
-
 }
 
